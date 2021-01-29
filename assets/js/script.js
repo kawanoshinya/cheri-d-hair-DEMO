d5d3a7391043d831d4706c1307e33dce88d17cb6
kawanoshinya <64684505+kawanoshinya@users.noreply.github.com>
2021-01-30 01:27:13 +0900

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 function hamburger() {
     document.getElementById('top').classList.toggle('bar--active');
     document.getElementById('middle').classList.toggle('bar--active');
@@ -14,6 +15,17 @@ document.getElementById('bg_layer').addEventListener('click', function () {
     hamburger();
 });
 
+$('a[href^="#"]').click(function () {
+    var speed = 500;
+    var headerH = $(".header").height();
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - headerH;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    hamburger();
+    return false;
+});
+
 let list = document.getElementsByClassName('nav_menu');
 
 for (let i = 0; i < list.length; i++) {
@@ -22,11 +34,19 @@ for (let i = 0; i < list.length; i++) {
     });
 }
 
-// $(function () {
-//     var height = $("#header").height();
-//     $("body").css("margin-top", height + 0);//0pxだけ余裕をもたせる
-// });
+$(function () {
+    var height = $("#header").height();
+    $("body").css("margin-top", height + 0);//0pxだけ余裕をもたせる
+});
 
 // スマホのアドレスバー対策
 $(".fv").css("height", $(window).height());
 
+var ua = navigator.userAgent.toLowerCase();
+var isMobile = /iphone/.test(ua) || /android(.+)?mobile/.test(ua);
+
+if (!isMobile) {
+    $('a[href^="tel:"]').on('click', function (e) {
+        e.preventDefault();
+    });
+}
